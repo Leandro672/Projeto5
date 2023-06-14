d4bd4e0927e1ca1c57e5ef629ed8cb0353fe8a9b
Leandro672 <leandro.gomes@sou.inteli.edu.br>
2023-06-14 09:59:18 -0300

    deploy
    deploy

--- src/Backend/app.js
+++ src/Backend/app.js
@@ -1,28 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const databaseConnection = require("scr/Backend/middlewares/databaseConnection.js")
+const databaseConnection = require("./middlewares/databaseConnection");
 const hostname = "127.0.0.1";
 const app = express();
 const dotenv = require("dotenv");
-dotenv.config({path: __dirname + "/.env"});
+const path = require("path");
+dotenv.config({ path: __dirname + "/.env" });
 app.use(express.json());
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
-app.use(express.static("scr/Frontend/index.html"));
-
-const PORT = 4000;
-
-const registerRoute = require("scr/Backend/routes/register")
-const loginRoute = require("scr/Backend/routes/login");
-const teacherRoute = require("scr/Backend/routes/teacher");
-const classRoute = require("scr/Backend/routes/class");
-const studentRoute = require("scr/Backend/routes/student");
-const lessonsRoute = require("scr/Backend/routes/lessons");
-const studentNotesRoute = require("scr/Backend/routes/studentNotes");
-const studentGradesRoute = require("scr/Backend/routes/studentGrades");
-
+app.use(express.static(path.join(__dirname, "/../Frontend")));
+const PORT = process.env.PORT;
+const registerRoute = require("./routes/register");
+const loginRoute = require("./routes/login");
+const teacherRoute = require("./routes/teacher");
+const classRoute = require("./routes/class");
+const studentRoute = require("./routes/student");
+const lessonsRoute = require("./routes/lessons");
+const studentNotesRoute = require("./routes/studentNotes");
+const studentGradesRoute = require("./routes/studentGrades");
 app.use(databaseConnection);
-
 app.use("/register", registerRoute);
 app.use("/login", loginRoute);
 app.use("/teacher", teacherRoute);
@@ -31,7 +28,9 @@ app.use("/student", studentRoute);
 app.use("/lessons", lessonsRoute);
 app.use("/studentNotes", studentNotesRoute);
 app.use("/studentGrades", studentGradesRoute);
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Frontend/index.html"));
+});
 app.listen(PORT, hostname, () => {
   console.log(`Servidor rodando em http://${hostname}:${PORT}/`);
 });
