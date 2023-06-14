1eef938875cc33467a0d21606dbbede3268b63eb
Leandro672 <leandro.gomes@sou.inteli.edu.br>
2023-06-14 09:26:52 -0300

    deploy
    deploy

--- src/Backend/app.js
+++ src/Backend/app.js
@@ -8,6 +8,7 @@ dotenv.config({path: __dirname + "/.env"});
 app.use(express.json());
 const cors = require('cors');
 app.use(cors());
+app.use(express.static("scr/frontend/index.html"));
 
 const PORT = process.env.PORT;
 
